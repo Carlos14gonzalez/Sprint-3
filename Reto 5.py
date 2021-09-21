3bb07f5c0fb6a450a659c87f32c8277571c976db
Carlos_Gonzalez <carlos14gonzalez@gmail.com>
2021-09-20 19:41:01 -0500

    Reto5

---
+++ Reto 5.py
@@ -0,0 +1,16 @@
+# ------- RETO 5 ------- #
+
+# Diseñar un algoritmo que permita a un docente obtener las notas finales de n estudiantes en una lista.
+# Diseñe una función llamada "Promedio", que permita al docente obtener la nota final de n estudiantes, sabiendo que el curso se conforma de 3 notas de igual peso,
+# usted deberá retornar una lista que sus elementos sean la nota final de cada estudiante.
+# Retorna la respuesta con el siguiente formato la salida sería  una lista por ejemplo [2.93, 3.57, 3.77, 3.37, 2.17, 2.67, 1.57]
+
+def promedio (Matriz):
+    lista []
+    n = len (Matriz)
+
+
+
+    lista.append (round(prom,2))
+    Return lista
+    
